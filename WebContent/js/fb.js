67188db9549973791e907cfedfddee873044960f
Rohini <rohini.thakar85@gmail.com>
2013-11-09 17:53:22 -0800

    1st commit

---
+++ WebContent/js/fb.js
@@ -0,0 +1,73 @@
+  
+(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=682447581775162";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+window.fbAsyncInit = function()
+        {
+            FB.init
+            (
+                {
+                    appId   : "682447581775162",
+                    status  : true,
+                    cookie  : true,
+                    oauth   : true
+                }
+            );
+        };
+
+        function login()
+        {
+            FB.login
+            (
+                function( response )
+                {
+                    if ( response.authResponse )
+                    {
+                        FB.api
+                        (
+                            "/me",
+                            function( response )
+                            {
+                            	  document.getElementById("fblogin").style.visibility = "hidden";  
+                            	  document.getElementById("login-user").style.visibility = "hidden";  
+                            	  document.getElementById("create-user").style.visibility = "hidden";  
+                            	  document.getElementById("fblogout").style.visibility = "visible";   	  
+                            	//  document.getElementById("fblogout").style.display = "block";   
+                            	  
+                                  document.getElementById( "profile_name" ).style.visibility = "visible";
+                                  document.getElementById( "profile_pic" ).style.visibility = "visible";
+
+                            	
+                                document.getElementById( "profile_name" ).innerHTML = response.name;
+                                document.getElementById( "profile_pic" ).src = "http://graph.facebook.com/" + response.id + "/picture";
+                            }
+                        )
+                    }
+                }
+            );
+        }
+
+        function logout()
+        {
+          document.getElementById("login-user").style.visibility = "visible";  
+      	  document.getElementById("create-user").style.visibility = "visible";  
+      	  
+      	  document.getElementById("fblogin").style.visibility = "visible";   	  
+    	  //document.getElementById("fblogout").style.visibility = "hidden";   	  
+      	  document.getElementById("fblogout").style.display = "none"; 
+        	
+            document.getElementById( "profile_name" ).style.visibility = "hidden";
+            document.getElementById( "profile_pic" ).style.visibility = "hidden";
+
+            
+            FB.logout(function(response) {
+            	  // user is now logged out
+            	});
+        }
+        
